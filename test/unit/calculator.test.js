329d50e0d212b97da59f815dd17c0ac58c4fbe77
woosh0901-sys <woosh0901@gmail.com>
2026-05-27 17:30:13 +0900

    feat: 곱하기 연산 추가

--- test/unit/calculator.test.js
+++ test/unit/calculator.test.js
@@ -1,5 +1,5 @@
 // 단위 테스트: 함수 하나를 격리해서 테스트
-import { add, subtract, calculate } from "../../src/calculator.js";
+import { add, subtract, multiply, calculate } from "../../src/calculator.js";
 describe("add", () => {
  test("두 양수를 더한다", () => {
  expect(add(2, 3)).toBe(5);
@@ -14,6 +14,14 @@ describe("subtract", () => {
  });
  test("음수 결과도 처리한다", () => {
  expect(subtract(3, 10)).toBe(-7);
+ });
+});
+describe("multiply", () => {
+ test("두 수를 곱한다", () => {
+ expect(multiply(3, 4)).toBe(12);
+ });
+ test("0과의 곱은 0이다", () => {
+ expect(multiply(7, 0)).toBe(0);
  });
 });
 describe("calculate", () => {
